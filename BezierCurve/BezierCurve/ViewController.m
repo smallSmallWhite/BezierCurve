d07d440db0307f7384863af427dd94388e92e806
admin <1395258209@qq.com>
2017-01-08 21:56:58 +0800

    贝塞尔曲线绘制

--- BezierCurve/BezierCurve/ViewController.m
+++ BezierCurve/BezierCurve/ViewController.m
@@ -14,45 +14,12 @@
 @end
 
 @implementation ViewController
-#pragma mark  直线
-- (IBAction)line:(id)sender {
-    
-//    [_bezierPath stroke];
-}
-#pragma mark  二阶曲线
-- (IBAction)curveOfOrder2:(id)sender {
-    
-//    UIBezierPath *bezierPath = [UIBezierPath bezierPath];
-//    [bezierPath removeAllPoints];
-    
-}
-#pragma mark 三阶曲线
-- (IBAction)curveOfOrder3:(id)sender {
-}
-#pragma mark  多阶曲线
-- (IBAction)curveOfOrderMany:(id)sender {
-}
-#pragma mark  矩形
-- (IBAction)rect:(id)sender {
-}
-#pragma mark  圆角矩形
-- (IBAction)cordiousRect:(id)sender {
-}
-#pragma mark 部分圆角矩形
-- (IBAction)partORadiousRect:(id)sender {
-}
-#pragma mark  圆
-- (IBAction)circle:(id)sender {
-}
-#pragma mark  弧
-- (IBAction)curve:(id)sender {
-}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 //    [self drawRect2:CGRectMake(100, 100, 100, 100)];
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 400, 400)];
     view.backgroundColor = [UIColor clearColor];
     [self.view addSubview:view];
     view.layer.delegate = self;
@@ -68,7 +35,21 @@
     //画直线
 //    [self drawRect];
     //画矩形
-    [self drawRect1];
+//    [self drawRect1];
+    //圆角矩形
+//    [self drawRect2];
+    //部分圆角矩形
+//    [self drawRect3];
+    //画圆
+//    [self drawRect4];
+    //画圆弧
+//    [self drawRect5];
+    //画二阶曲线
+//    [self drawRect6];
+    //画三阶曲线
+//    [self drawRect7];
+    //画多阶曲线
+    [self drawRect8];
     UIGraphicsPopContext();
     
 }
@@ -95,13 +76,125 @@
     // 设置线的填充色
     [[UIColor redColor] setStroke];
     // 新建一个bezier对象，此对象用于绘制矩形，需要传入绘制的矩形的Frame
-    _bezierPath = [UIBezierPath bezierPathWithRect:CGRectMake(10, 10, 50, 50)];
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithRect:CGRectMake(10, 10, 50, 50)];
     // 设置线宽度
-    _bezierPath.lineWidth = 10;
+    bezierPath.lineWidth = 10;
     // 设置线两头样式
-    _bezierPath.lineCapStyle = kCGLineCapRound;
+    bezierPath.lineCapStyle = kCGLineCapRound;
     // 开始绘制
-    [_bezierPath stroke];
+    [bezierPath stroke];
+}
+//圆角矩形
+- (void)drawRect2 {
+    
+    // 设置线的填充色
+    [[UIColor redColor] setStroke];
+    // 新建一个bezier对象，此对象用于绘制一个圆角矩形
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(10, 10, 50, 50)cornerRadius:10];
+    // 设置线宽度
+    bezierPath.lineWidth = 10;
+    // 开始绘制
+    [bezierPath stroke];
+}
+//部分圆角矩形
+- (void)drawRect3 {
+    
+    // 设置线的填充色
+    [[UIColor redColor] setStroke];
+    // 新建一个bezier对象，此对象用于绘制一个部分圆角的矩形，左上、右下
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(10, 10, 50, 50) byRoundingCorners:UIRectCornerTopLeft | UIRectCornerBottomRight cornerRadii:CGSizeMake(10, 10)];
+    // 设置线宽度
+    bezierPath.lineWidth = 10;
+    // 开始绘制
+    [bezierPath stroke];
+}
+//画圆
+- (void)drawRect4 {
+    
+    // 设置线的填充色
+    [[UIColor redColor] setStroke];
+    // 新建一个bezier对象，此对象用于绘制内切圆，需要传入绘制内切圆的矩形的Frame
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(10, 10, 50, 50)];
+    // 设置线宽度
+    bezierPath.lineWidth = 10;
+    // 设置线两头样式
+    bezierPath.lineCapStyle = kCGLineCapRound;
+    // 开始绘制
+    [bezierPath stroke];
+}
+//圆弧
+-(void)drawRect5 {
+    
+    // 设置线的填充色
+    [[UIColor redColor] setStroke];
+    // 新建一个bezier对象，此对象用于绘制一个圆弧
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithArcCenter:CGPointMake(150, 150)radius:110 startAngle:0 endAngle:M_PI_2 clockwise:NO];
+    // 设置线宽度
+    bezierPath.lineWidth = 10;
+    // 开始绘制
+    [bezierPath stroke];
+}
+//二阶曲线
+- (void)drawRect6 {
+    
+    // 设置线的填充色
+    [[UIColor redColor] setStroke];
+    // 新建一个bezier对象
+    UIBezierPath *bezierPath = [UIBezierPath bezierPath];
+    // 设置线宽度
+    bezierPath.lineWidth = 10;
+    // 设置线两头样式
+    bezierPath.lineCapStyle = kCGLineCapRound;
+    // 设置起点、终点坐标
+    [bezierPath moveToPoint:CGPointMake(100, 100)];
+    [bezierPath addQuadCurveToPoint:CGPointMake(200, 200) controlPoint:CGPointMake(300, 0)];
+    // 开始绘制
+    [bezierPath stroke];
+}
+//三阶曲线
+- (void)drawRect7{
+    
+    // 设置线的填充色
+    [[UIColor redColor] setStroke];
+    // 新建一个bezier对象
+    UIBezierPath *bezierPath = [UIBezierPath bezierPath];
+    // 设置线宽度
+    bezierPath.lineWidth = 10;
+    // 设置线两头样式
+    bezierPath.lineCapStyle = kCGLineCapRound;
+    // 设置起点、终点坐标
+    [bezierPath moveToPoint:CGPointMake(100, 100)];
+    [bezierPath addCurveToPoint:CGPointMake(200, 200) controlPoint1:CGPointMake(150, 0) controlPoint2:CGPointMake(150, 300)];
+    // 开始绘制
+    [bezierPath stroke];
+}
+//多阶曲线
+- (void)drawRect8 {
+    
+    // 设置线的填充色
+    [[UIColor redColor] setStroke];
+    // 新建一个bezier对象
+    UIBezierPath *bezierPath = [UIBezierPath bezierPath];
+    // 设置线宽度
+    bezierPath.lineWidth = 10;
+    // 设置线两头样式
+    bezierPath.lineCapStyle = kCGLineCapRound;
+    // 设置起点、终点坐标
+    [bezierPath moveToPoint:CGPointMake(100, 100)];
+    [bezierPath addCurveToPoint:CGPointMake(200, 200)
+                  controlPoint1:CGPointMake(150, 0)
+                  controlPoint2:CGPointMake(150, 300)];
+    // 创建第二条贝塞尔曲线
+    UIBezierPath *bezierPath2 = [UIBezierPath bezierPath];
+    // 设置起点、终点坐标
+    [bezierPath2 moveToPoint:CGPointMake(200, 200)];
+    [bezierPath2 addCurveToPoint:CGPointMake(290, 290)
+                   controlPoint1:CGPointMake(250, 0)
+                   controlPoint2:CGPointMake(250, 300)];
+    // 将第二条线，加到第一条线上面去
+    [bezierPath appendPath:bezierPath2];
+    // 开始绘制
+    [bezierPath stroke];
 }
 
 - (void)didReceiveMemoryWarning {
