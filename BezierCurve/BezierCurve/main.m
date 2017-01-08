f0bfbd1475938061522ecc0ecf3b092df9d9969c
admin <1395258209@qq.com>
2017-01-08 12:08:19 +0800

    创建一个工程

---
+++ BezierCurve/BezierCurve/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BezierCurve
+//
+//  Created by admin on 17/1/8.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
